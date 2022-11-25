d805c5085784e0d31177e61c82117cd76166cac5
Rociliu <mrrosa.carrasco@gmail.com>
2022-11-25 23:50:22 +0100

    iteracion#1

---
+++ Iteracion1-variables.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+console.log (z);
---
+++ Iteracion3-operadores.js
@@ -0,0 +1,19 @@
+"use strict";
+let mult = 10*5;
+console.log (mult);
+
+let div = 10/2;
+console.log (div);
+
+let resto = 15%9;
+console.log (resto);
+
+
+let y = 10;
+let z = 5;
+
+let x = y + z;
+console.log(x);
+
+ x = y * z;
+console.log(x);

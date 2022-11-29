a4eafb0b875ea6a8c83263edb6cb7d5774debce4
Rociliu <mrrosa.carrasco@gmail.com>
2022-11-29 20:37:14 +0100

    iteracion#4

--- Iteracion3-operadores.js
+++ Iteracion3-operadores.js
@@ -1,20 +1,40 @@
 "use strict";
 
+/*1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.*/
 let mult = 10*5;
 console.log (mult);
 
+
+/*1.2 Divide 10 por 2 y muestra el resultado en un alert.*/
 let div = 10/2;
 console.log (div);
 
+
+/*1.3 Muestra mediante un alert el resto de dividir 15 por 9.*/
 let resto = 15%9;
 console.log (resto);
 
 
+/*1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.*/
 let y = 10;
 let z = 5;
 
 let x = y + z;
 console.log(x);
 
- x = y * z;
+/*1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.*/
+let y = 10;
+let z = 5;
+
+x = y * z;
 console.log(x);
+
+
+
+
+
+
+
+
--- Iteracion5-condicionales.js
+++ Iteracion5-condicionales.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 const number1 = 10;
 const number2 = 20;
 const number3 = 2;
